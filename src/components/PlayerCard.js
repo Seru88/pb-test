84337a9d5b0a7cf60856c7758a97a5299f9db1b5
Sergio Nizama <sergio.nizama@outlook.com>
2019-06-17 04:01:11 -0400

    removed usage of card component, and implementing own with boxes

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,13 +1,6 @@
 import {
   Avatar,
   Box,
-  Button,
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
   Typography,
 } from '@material-ui/core';
 import { ThemeProvider, useTheme } from '@material-ui/styles';
@@ -15,14 +8,6 @@ import { ThemeProvider, useTheme } from '@material-ui/styles';
 import Flag from 'react-world-flags';
 import React from 'react';
 import styled from 'styled-components';
-
-const StyledCard = styled(Card)`
-  // display: flex;
-  // flex-direction: column;
-  width: 320px;
-  min-height: 200px;
-  margin: ${({ theme }) => `${theme.spacing(0.5)}px`};
-`;
 
 const StyledAvatar = styled(Avatar)`
   // margin: ${({ theme }) => `0 auto ${theme.spacing(1.5)}px auto`};
@@ -47,38 +32,43 @@ export default ({ player }) => {
   };
   return (
     <ThemeProvider theme={theme}>
-      <StyledCard>
-        <CardActionArea>
-          <CardContent>
-            <Box
-              display="flex"
-              justifyContent="flex-start"
-              alignItems="center"
-              marginBottom={1.5}
+      <Box
+        className="player-card"
+        width={320}
+        minHeight={200}
+        m={1.25}
+        p={2}
+        boxShadow={3}
+        borderRadius={25}
+      >
+        <Box className="player-info">
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            marginBottom={1.5}
+          >
+            <StyledAvatar
+              selected={selected}
+              src={player.avatarUrl}
+              // can't seem to set these props with styled-components for some reason 🙁
+              style={{ width: 80, height: 80 }}
+              imgProps={{ align: 'left' }}
+              onClick={onSelect}
+            />
+            <Typography
+              className="nickname"
+              variant="h5"
+              // style={{ margin: '0 auto' }}
             >
-              <StyledAvatar
-                selected={selected}
-                src={player.avatarUrl}
-                // can't seem to set these props with styled-components for some reason 🙁
-                style={{ width: 80, height: 80 }}
-                imgProps={{ align: 'left' }}
-                onClick={onSelect}
-              />
-              <Typography
-                className="nickname"
-                variant="h5"
-                // style={{ margin: '0 auto' }}
-              >
-                {player.nickname}
-                {/* <Flag code={player.country} height={22} width={32} /> */}
-              </Typography>
-            </Box>
-            <Typography className="message" variant="caption">
-              {player.message}
+              {player.nickname}
             </Typography>
-          </CardContent>
-        </CardActionArea>
-      </StyledCard>
+          </Box>
+          <Typography className="message" variant="caption">
+            {player.message}
+          </Typography>
+        </Box>
+      </Box>
     </ThemeProvider>
   );
 };
