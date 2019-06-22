5023d38ac6d4bf62b5f784a5730012f02aab57b3
Sergio Nizama <sergio.nizama@outlook.com>
2019-06-22 13:01:28 -0400

    refactored VotingStepper

--- src/components/VotingStepper.js
+++ src/components/VotingStepper.js
@@ -12,7 +12,6 @@ const Label = styled(Grid)`
 
 const StyledAvatar = styled(Avatar)`
   transition: ${({ theme }) => theme.transitions.create(['border'])};
-
   border: ${({ theme, chosen }) =>
     chosen ? `4px solid ${theme.palette.primary.main}` : `none`};
 `;
@@ -20,10 +19,35 @@ const StyledAvatar = styled(Avatar)`
 export default ({ players, onSubmit }) => {
   const theme = useTheme();
 
+  const selections = [];
+  for (let i = 0; i < 3; i++) {
+    const avatar = (
+      <Grid key={i} item xs={3} md={2}>
+        <Box height="100%" display="flex" alignItems="center">
+          <StyledAvatar
+            style={{ width: 50, height: 50 }}
+            src={players[i] ? players[i].avatarUrl : ''}
+            chosen={players.length > i ? 1 : 0}
+          >
+            ???
+          </StyledAvatar>
+          <Typography
+            style={{ paddingLeft: theme.spacing(1) }}
+            variant="h6"
+            color="textSecondary"
+          >
+            {players.length > i ? players[i].nickname : `Player ${i+1}`}
+          </Typography>
+        </Box>
+      </Grid>
+    );
+    selections.push(avatar);
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Box
-        height={{ sm: 180,  md:73}}
+        height={{ sm: 180, md: 73 }}
         width="100%"
         position="sticky"
         bottom={0}
@@ -32,74 +56,26 @@ export default ({ players, onSubmit }) => {
       >
         <Grid container justify="center" alignContent="space-between">
           <Label item xs={12} md={3}>
-            <Box fontSize="2.2rem" textAlign="center" color="text.secondary">
+            <Typography align="center" color="textSecondary" variant="h6">
               Casting a vote for:
-            </Box>
-            <Box
-              fontSize="1.65rem"
-              textAlign="center"
-              mt={-1}
-              color="#b4b7ba"
+            </Typography>
+            <Typography
+              align="center"
+              variant="subtitle1"
+              style={{ color: '#b4b7ba', marginTop: -10 }}
             >
               *CANNOT vote accross regions.
-            </Box>
+            </Typography>
           </Label>
-          <Grid item xs={3} md={2}>
-            <Box height="100%" display="flex" alignItems="center">
-              <StyledAvatar
-                style={{ width: 50, height: 50 }}
-                src={players[0] ? players[0].avatarUrl : ''}
-                chosen={players.length >= 1 ? 1 : 0}
-              >
-                ???
-              </StyledAvatar>
-              <Typography
-                style={{ paddingLeft: theme.spacing(1) }}
-                variant="h6"
-                color="textSecondary"
-              >
-                {players.length >= 1 ? players[0].nickname : 'Player 1'}
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={3} md={2}>
-            <Box height="100%" display="flex" alignItems="center">
-              <StyledAvatar
-                style={{ width: 50, height: 50 }}
-                src={players[1] ? players[1].avatarUrl : ''}
-                chosen={players.length >= 2 ? 1 : 0}
-              >
-                ???
-              </StyledAvatar>
-              <Typography
-                style={{ paddingLeft: theme.spacing(1) }}
-                variant="h6"
-                color="textSecondary"
-              >
-                {players.length >= 2 ? players[1].nickname : 'Player 2'}
-              </Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={3} md={2}>
-            <Box height="100%" display="flex" alignItems="center">
-              <StyledAvatar
-                style={{ width: 50, height: 50 }}
-                src={players[2] ? players[2].avatarUrl : ''}
-                chosen={players.length >= 3 ? 1 : 0}
-              >
-                ???
-              </StyledAvatar>
-              <Typography
-                style={{ paddingLeft: theme.spacing(1) }}
-                variant="h6"
-                color="textSecondary"
-              >
-                {players.length >= 3 ? players[2].nickname : 'Player 3'}
-              </Typography>
-            </Box>
-          </Grid>
+          {selections}
           <Grid item xs={12} md={2}>
-            <Box height="100%" display="flex" alignItems="center" p={2} justifyContent="center">
+            <Box
+              height="100%"
+              display="flex"
+              alignItems="center"
+              p={2}
+              justifyContent="center"
+            >
               <StyledButton
                 selected={players.length === 3}
                 onClick={onSubmit}
